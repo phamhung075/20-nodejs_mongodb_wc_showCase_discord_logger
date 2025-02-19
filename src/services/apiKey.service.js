42199108bcdb10ff5753447059f6ee7b320cb978
Dai Hung PHAM <daihung.pham@gmail.com>
2025-02-19 18:38:10 +0100

    a

--- src/services/apiKey.service.js
+++ src/services/apiKey.service.js
@@ -1,20 +1,21 @@
-'use strict'
+"use strict";
 
 const apiKeyModel = require("../models/apiKey.model");
-const crypto = require ('crypto');
+const crypto = require("crypto");
 
+const findById = async (key) => {
+  // //create Api for test
+  //   const newKey = await apiKeyModel.create({
+  //     key: crypto.randomBytes(64).toString("hex"),
+  //     permissions: ["0000"],
+  //   });
+  //   console.log(newKey);
+  // //end Api for test
 
-const findById = async ( key ) => {
-    // //create Api for test
-    // const newKey = await apiKeyModel.create({key: crypto.randomBytes(64).toString('hex'), permissions: ['0000']});
-    // console.log(newKey);
-    // //end Api for test
-
-    const objKey = await apiKeyModel.findOne({ key, status: true}).lean();
-    return objKey
-}
-
+  const objKey = await apiKeyModel.findOne({ key, status: true }).lean();
+  return objKey;
+};
 
 module.exports = {
-    findById
-}
+  findById,
+};
